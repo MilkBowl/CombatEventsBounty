0d96e81272478c60a45e8bbc7babdcf3358007ef
sleaker <sleaker@sleak-pc>
2011-06-08 00:34:07 -0700

    added WorldLoadEvent class

---
+++ src/com/sleaker/KillaKreditz/KillaKreditzWorldLoadEvent.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.sleaker.KillaKreditz;
+
+import org.bukkit.event.world.WorldListener;
+import org.bukkit.event.world.WorldLoadEvent;
+
+/**
+ * @author sleaker
+ *
+ * Detects when a new world is loaded in and runs the configuration loader for the world.
+ */
+public class KillaKreditzWorldLoadEvent extends WorldListener {
+
+    public class NoSpawnWorldLoadEvent extends WorldListener{
+
+        public final KillaKreditz plugin;
+
+        public NoSpawnWorldLoadEvent(KillaKreditz instance) {
+            plugin = instance;
+        }
+
+        public void onWorldLoad(WorldLoadEvent event) {
+            KillaKreditz.setupWorld(event.getWorld().getName());
+        }
+    }
+}
